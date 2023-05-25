d20d5aa3d1a798a9743b1430a2a4050489b54995
Gyles Polloso <gyles.samuelpolloso@jarden.co.nz>
2023-05-26 09:15:08 +1200

    Update domain unit tests

--- packages/domain/src/usecase/log/save-logs.test.ts
+++ packages/domain/src/usecase/log/save-logs.test.ts
@@ -1,6 +1,8 @@
 import { LogItem } from '../../entity/log-item'
+import { Payload } from '../../entity/payload'
 import { DomainException } from '../exception/domain-exception'
 import { SaveLogs } from './save-logs'
+import { v4 } from 'uuid'
 
 describe('Save Logs use case', () => {
   const logGateway = {
@@ -13,10 +15,39 @@ describe('Save Logs use case', () => {
     jest.resetAllMocks()
   })
 
-  it('should throw domain exception when saving logs given no log item', () => {
-    const useCase = new SaveLogs<Array<LogItem>, string, string>(logGateway)
+  describe('saveLogs', () => {
+    it('should throw domain exception when saving logs given no log item', () => {
+      const useCase = new SaveLogs<Array<LogItem>, string, string>(logGateway)
 
-    expect(() => useCase.saveLogs([])).toThrow(DomainException)
-    expect(() => useCase.saveLogs([])).toThrow('Cannot save empty logs.')
+      expect(() => useCase.saveLogs([])).toThrow(DomainException)
+      expect(() => useCase.saveLogs([])).toThrow('Cannot save empty logs.')
+    })
+
+    it('should allow save when saving logs given log items', () => {
+      const useCase = new SaveLogs<Array<LogItem>, string, string>(logGateway)
+
+      const logs = [
+        new LogItem(
+          v4(),
+          new Date(),
+          v4(),
+          'GET',
+          '/order/32e1bbc1-74f9-42bb-8ee5-c1efd819ce66',
+          undefined,
+        ),
+        new LogItem(
+          v4(),
+          new Date(),
+          v4(),
+          'POST',
+          '/order',
+          new Payload(v4(), v4(), 5),
+        ),
+      ]
+
+      useCase.saveLogs(logs)
+
+      expect(logGateway.saveLogs).toBeCalledWith(logs)
+    })
   })
 })
---
+++ packages/domain/src/usecase/log/search-logs.test.ts
@@ -0,0 +1,72 @@
+import { v4 } from 'uuid'
+import { LogItem } from '../../entity/log-item'
+import { SearchLogs } from './search-logs'
+import { Payload } from '../../entity/payload'
+
+describe('Search Logs use case', () => {
+  const logGateway = {
+    getLogById: jest.fn(),
+    getLogs: jest.fn(),
+    saveLogs: jest.fn(),
+  }
+
+  beforeEach(async () => {
+    jest.resetAllMocks()
+  })
+
+  describe('searchById', () => {
+    it('should return item when searching by id given an existing log item', () => {
+      const log = new LogItem(
+        v4(),
+        new Date(),
+        v4(),
+        'GET',
+        '/order/32e1bbc1-74f9-42bb-8ee5-c1efd819ce66',
+        undefined,
+      )
+      logGateway.getLogById.mockReturnValueOnce(log)
+
+      const useCase = new SearchLogs<Array<LogItem>, string, string>(logGateway)
+      const result = useCase.searchLogById(log.id)
+
+      expect(logGateway.getLogById).toBeCalledWith(log.id)
+      expect(result).toBe(log)
+    })
+  })
+
+  describe('searchByQuery', () => {
+    it('should return items when searching by query given a existing matches', () => {
+      const logs = [
+        new LogItem(
+          v4(),
+          new Date(),
+          v4(),
+          'GET',
+          '/order/32e1bbc1-74f9-42bb-8ee5-c1efd819ce66',
+          undefined,
+        ),
+        new LogItem(
+          v4(),
+          new Date(),
+          v4(),
+          'POST',
+          '/order',
+          new Payload(v4(), v4(), 5),
+        ),
+      ]
+      logGateway.getLogs.mockReturnValueOnce(logs)
+
+      const useCase = new SearchLogs<Array<LogItem>, string, string>(logGateway)
+      const result = useCase.searchByQuery(
+        'request',
+        'userId==5087d5bf-4f78-413b-b700-620a24306d56',
+      )
+
+      expect(logGateway.getLogs).toBeCalledWith(
+        'request',
+        'userId==5087d5bf-4f78-413b-b700-620a24306d56',
+      )
+      expect(result).toBe(logs)
+    })
+  })
+})
